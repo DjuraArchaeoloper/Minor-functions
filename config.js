5447451fc3d5e497514f673a39189b71fcf6a9ca
DjuraArchaeoloper <73395831+DjuraArchaeoloper@users.noreply.github.com>
2021-03-18 18:51:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ var max = 1000;
 var array = [25, "Djura", 57, 94, "Djuradj", 4, "Frontend", 2];
 var string = "Today, it is February 25, 2021, the pandemic started almost 9 months ago.";
 var arrayOfStrings = ["Djur","Dj", "Dju", "Djuradj", "Djura"];
-var type = 9;
+var type = 0;
 
 export {min, max, array, string, arrayOfStrings, type};
